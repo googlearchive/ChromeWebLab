06202346dbbb1d2a0e8a5211f5912f8c5586deaa
jakechampion <jakechampion.jake2@gmail.com>
2013-08-26 17:01:08 +0100

    update pre-commit hook

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,21 +1,39 @@
 #!/bin/sh
-# stash unstaged changes, run jsbeautifier task and restore stashed files
+
+# check for how many uncommitted changes we have
+# stash changes
+# run grunt task 
+# restore stashed files if anything was stashed
+# exit with error if grunt fails
 
 NAME=$(git branch | grep '*' | sed 's/* //')
+
+echo "Running pre-commit hook on: " $NAME
 
 # don't run on rebase
 if [ $NAME != '(no branch)' ]
 then
-  git stash -q --keep-index
-  grunt jsbeautifier
+  
+  CHANGES=$(git diff --numstat | wc -l)
+  CHANGES_CACHED=$(git diff --cached --numstat | wc -l)
+  TOTAL_CHANGES=$(($CHANGES + $CHANGES_CACHED))
+
+  git stash -k   # the "-k" makes git stash all changes, staged & unstaged 
+  grunt 
 
   RETVAL=$?
 
-  if [ $RETVAL -ne 0 ]
+  if [ $TOTAL_CHANGES -ne "0" ]
   then
+    echo "Popping" $TOTAL_CHANGES "changes off the stack..."
+    git stash pop -q
+  fi      
+
+  if [ $RETVAL -ne 0 ] 
+  then
+    echo "Grunt task failed, exiting..."
     exit 1
   fi
 
-  git add .
-  git stash pop -q
+  echo "Complete."
 fi
